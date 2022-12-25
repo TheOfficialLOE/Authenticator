dcd7cbdec4cf0b5b6ad14138c61529fe0521ea76
TheOfficialLOE <max.shady1611@gmail.com>
2022-12-25 20:25:20 +0330

    Change app font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,29 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sans": [
+          "Inter",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ]
+      }
+    }
   },
   plugins: [],
 }
