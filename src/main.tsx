c04b02e532834f7d512fea4a1e0c3a74e5ac4f06
TheOfficialLOE <max.shady1611@gmail.com>
2023-01-20 13:59:28 +0330

    ground up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { CssVarsProvider } from "@mui/joy";
 import App from "./App";
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <CssVarsProvider>
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>,
 )
