826c00ba88ef05d68f2b91cf17336d8ccb2a7ba5
TheOfficialLOE <max.shady1611@gmail.com>
2023-01-20 15:49:03 +0330

    set default dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { CssVarsProvider } from "@mui/joy";
+import { CssVarsProvider as JoyUIProvider, extendTheme } from "@mui/joy";
 import App from "./App";
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <CssVarsProvider>
-      <App />
-    </CssVarsProvider>
+    <JoyUIProvider
+      defaultMode="dark"
+      theme={extendTheme({cssVarPrefix: 'demo'})}
+      colorSchemeSelector="#demo_dark-mode-by-default"
+      modeStorageKey="demo_dark-mode-by-default"
+      disableNestedContext
+    >
+      <div id="demo_dark-mode-by-default">
+        <App/>
+      </div>
+    </JoyUIProvider>
   </React.StrictMode>,
 )
