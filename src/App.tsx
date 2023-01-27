67e198ae76512d3a678655e8624ad9c459f601e1
loe <max.shady1611@gmail.com>
2023-01-28 00:23:50 +0330

    Separate icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import {
-  Box,
+  Box, BreakpointOverrides,
   Card, CircularProgress,
   IconButton,
   List,
@@ -11,6 +11,9 @@ import {
   Typography
 } from "@mui/joy";
 import {useEffect, useState} from "react";
+import createBreakpoints from "@mui/system/createTheme/createBreakpoints";
+import CopyIcon from "./components/icons/CopyIcon";
+import ThreeDotsIcon from "./components/icons/ThreeDotsIcon";
 
 /*
 * responsive
@@ -36,7 +39,7 @@ const App = () => {
     }
   });
 
-  console.log(timer);
+  const breakpoints = createBreakpoints({  });
 
   // @ts-ignore
   const handleClick = (event) => {
@@ -66,9 +69,7 @@ const App = () => {
                 width: 16,
                 height: 16
               }}>
-                <svg width={16} height={16} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
-                </svg>
+                <CopyIcon />
               </IconButton>
               <IconButton size="sm" variant="plain" sx={{
                 width: 16,
@@ -78,9 +79,7 @@ const App = () => {
                           aria-controls={open ? 'positioned-demo-menu' : undefined}
                           aria-haspopup="true"
               >
-                <svg width={16} height={16} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
-                </svg>
+                <ThreeDotsIcon />
               </IconButton>
               <Menu
                 id="positioned-demo-menu"
